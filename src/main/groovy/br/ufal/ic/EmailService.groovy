6cfd7b49662067bd1efbde7f93759f4deb1950ff
Romero Malaquias <romero.malaquias@gmail.com>
2015-07-21 14:09:37 -0300

    Email service funcional

---
+++ src/main/groovy/br/ufal/ic/EmailService.groovy
@@ -0,0 +1,108 @@
+package br.ufal.ic
+
+import br.ufal.ic.commons.ServerRPC
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+import javax.script.ScriptContext
+import javax.script.ScriptEngine
+import javax.script.ScriptEngineManager
+import javax.script.SimpleScriptContext
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+
+
+
+class EmailService extends ServerRPC {
+    EmailService(String exchangeName, String key) {
+        super(exchangeName, key)
+    }
+
+    public boolean sendEmail(email) {
+        final String username = "email.service.test123@gmail.com";
+        final String password = "service123";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            println session
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("email.service.test123@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(email.to));
+            message.setSubject(email.subject);
+            message.setText(email.body);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+            return false;
+        }
+        return true;
+    }
+
+    String doWork (String message){
+        def email;
+        try {
+            def jsonSlurper = new JsonSlurper()
+            email = jsonSlurper.parseText(message)
+
+
+        } catch(Exception e) {
+            email['__result'] = "INVALID_FORMAT"
+        }
+
+
+        try {
+            if(email.subject && email.body && email.to) {
+                if (sendEmail(email)) {
+                    email['__result'] = "SENDED"
+                } else {
+                    email['__result'] = "FAIL"
+                }
+            } else {
+                email['__result'] = "INVALID_FORMAT"
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace()
+            email['__result'] = "INVALID_FORMAT"
+        }
+        return JsonOutput.toJson(email)
+    }
+
+    public static void main(String[] argv) {
+        EmailService emailServer = new EmailService("EXCHANGE", "email");
+
+
+    }
+
+
+}
+
+
+
+
+
+
---
+++ src/main/groovy/br/ufal/ic/EmailTest.groovy
@@ -0,0 +1,40 @@
+package br.ufal.ic
+
+import br.ufal.ic.commons.ClientRPC
+
+/**
+ * Created by huxley on 18/07/15.
+ */
+class EmailTest extends ClientRPC {
+    private String response
+
+    EmailTest(String exchangeName, String key) {
+        super(exchangeName, key);
+    }
+
+    void doWork (String message){
+        response = message
+        println message
+    }
+
+    public String getResponse() {
+        return response
+    }
+
+    public boolean getLoop() {
+        return this.listen
+    }
+
+    public static void main(String[] argv) {
+        EmailTest fibonacciRpc = new EmailTest("EXCHANGE", "email");
+        fibonacciRpc.start();
+        fibonacciRpc.call('{"to":"romero.malaquias@gmail.com", "subject": "Test email service", "body": "tchubaruba"}');
+
+    }
+
+}
+
+
+
+
+
